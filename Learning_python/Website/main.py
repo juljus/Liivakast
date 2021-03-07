502611fdc766e0093d3842f4e40efbdeba291a1d
juljus <juljus@gmail.com>
2021-03-07 19:54:31 +0200

    untest

--- Learning_python/Website/main.py
+++ Learning_python/Website/main.py
@@ -7,9 +7,11 @@ app = Flask(__name__)
 def home():
     return "Hello, this is the main page! <h1>HELLO<h1>"
 
+
 @app.route("/<name>")
 def user(name):
     return f"Hello {name}!"
+
 
 @app.route("/admin")
 def admin():
